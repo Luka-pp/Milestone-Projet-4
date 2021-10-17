522b058804bb649ad79d4e861584e8655421d1cb
luka_pp <luka_pp@hotmail.com>
2021-10-17 16:58:37 +0100

    fixed typo on bucket name

--- Milestone_Project_4/settings.py
+++ Milestone_Project_4/settings.py
@@ -148,7 +148,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'majestic-plants'
+    AWS_STORAGE_BUCKET_NAME = 'majesticplants'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
