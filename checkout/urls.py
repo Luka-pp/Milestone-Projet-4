2bcc64c93fdf756a8af4c734d841ed4ed87fac86
luka_pp <luka_pp@hotmail.com>
2021-10-23 23:00:05 +0100

    refactored and checked over the code in checkout app

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,5 +1,3 @@
-
-from django.contrib import admin
 from django.urls import path
 from . import views
 from .webhooks import webhook
@@ -8,5 +6,5 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
     path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
-    path('wh/', webhook, name='webhook')
+    path('wh/', webhook, name='webhook'),
 ]
