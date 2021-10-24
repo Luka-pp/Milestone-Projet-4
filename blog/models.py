1cb8bd50e1f43fc38ffff83c685bd0db63d1a3d0
luka_pp <luka_pp@hotmail.com>
2021-10-24 23:34:23 +0100

    work done on Readme.md

--- blog/models.py
+++ blog/models.py
@@ -1,22 +1,24 @@
 from django.db import models
+from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
 
-class BlogPost(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    body_text = models.TextField(default=None, blank=True, null=True)
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
 
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
-created_at = models.DateTimeField(auto_now_add=True, blank=True)
-updated_at = models.DateTimeField(auto_now=True, blank=True)
+    class Meta:
+        ordering = ['-created_on']
 
-
-class Comments(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
-
-
-comment = models.TextField(default=None, blank=True, null=True)
+    def __str__(self):
+        return self.title
