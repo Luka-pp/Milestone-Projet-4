f2e18fb6716f2e5cd225ca7b847ab9b9f2ac4c1c
luka_pp <luka_pp@hotmail.com>
2021-10-24 23:49:39 +0100

    added blog urls and changed views and models for it

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
-from django.urls import path
 from . import views
-
+from django.urls import path
 
 urlpatterns = [
-    path('', views.blog, name='blog')
+    path('', views.PostList.as_view(), name='blog'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
